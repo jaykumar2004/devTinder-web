80b40d25dbc345ecd9e1a9a61c9f125a1b2c2e33
Jay Kumar Jangid <106320129+jaykumar2004@users.noreply.github.com>
2025-04-16 15:22:24 +0530

    setting up the project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='font-bold italic '>Hello</h1>
+    </>
+  )
+}
+
+export default App
