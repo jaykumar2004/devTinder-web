e13cd1c92c8a9576c0c3e223914acf4d75f205d2
Jay Kumar Jangid <106320129+jaykumar2004@users.noreply.github.com>
2025-04-18 15:30:16 +0530

    redirected to login if not authenticated

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,8 +6,8 @@ import { useNavigate } from "react-router-dom";
 import { BASE_URL } from "../utils/constants";
 
 const Login = () => {
-  const [emailId, setEmailId] = useState("jaykumar@gmail.com");
-  const [password, setPassword] = useState("Jaykumar@2004");
+  const [emailId, setEmailId] = useState("rahul@gmail.com");
+  const [password, setPassword] = useState("Rahul@2004");
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
