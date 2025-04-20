d910e4eb7e12104110c0ff64d459b28db765554f
Jay Kumar Jangid <106320129+jaykumar2004@users.noreply.github.com>
2025-04-20 15:13:23 +0530

    building feed api

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect } from "react";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addFeed } from "../utils/feedSlice";
 
 function Feed() {
-  return (
-    <div>This is a feed page</div>
-  )
+  const feed = useSelector((store) => store.feed);
+  const dispatch = useDispatch();
+
+  const getFeed = async () => {
+    if (feed) return;
+    try {
+      const res = await axios.get(BASE_URL + "/feed", {
+        withCredentials: true,
+      });
+      dispatch(addFeed(res.data));
+    } catch (err) {
+      //handle the error
+    }
+  };
+  useEffect(() => {
+    getFeed();
+  }, []);
+
+  return <div>Feed</div>;
 }
 
 export default Feed;
