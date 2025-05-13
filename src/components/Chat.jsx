cacd21ca7aa5d00a5071c8b965eb872f4b7c0649
Jay Kumar Jangid <106320129+jaykumar2004@users.noreply.github.com>
2025-05-14 00:28:22 +0530

    added bg image

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { Socket } from "socket.io-client";
 import { createSocketConnection } from "../utils/socket";
 import { useSelector } from "react-redux";
 
@@ -12,9 +11,8 @@ const Chat = () => {
   const userId = user?._id;
 
   useEffect(() => {
-    if (!userId) {
-      return;
-    }
+    if (!userId) return;
+
     const socket = createSocketConnection();
     socket.emit("joinChat", {
       firstName: user.firstName,
@@ -22,10 +20,10 @@ const Chat = () => {
       targetUserId,
     });
 
-    socket.on("messageReceived",({firstName, text })=>{
-        console.log(firstName, " : ", text);
-        setMessages((messages)=>[...messages,{firstName, text}]);
-    })
+    socket.on("messageReceived", ({ firstName, text }) => {
+      console.log(firstName, " : ", text);
+      setMessages((messages) => [...messages, { firstName, text }]);
+    });
 
     return () => {
       socket.disconnect();
@@ -35,7 +33,7 @@ const Chat = () => {
   const sendMessage = () => {
     const socket = createSocketConnection();
     socket.emit("sendMessage", {
-      firstName : user.firstName,
+      firstName: user.firstName,
       userId,
       targetUserId,
       text: newMessage,
@@ -44,31 +42,46 @@ const Chat = () => {
   };
 
   return (
-    <div className="w-3/4 mx-auto border border-gray-600 m-5 h-[70vh] flex flex-col">
-      <h1 className="p-5 border-b border-gray-600">Chat</h1>
-      <div className="flex-1 overflow-scroll p-5">
-        {messages.map((msg, index) => {
-          return (
+    <div
+      className="min-h-screen w-full flex items-center justify-center"
+      style={{
+        backgroundImage:
+          'url("https://sdmntprnorthcentralus.oaiusercontent.com/files/00000000-71d4-622f-a76e-31ed2f7920ec/raw?se=2025-05-13T19%3A50%3A09Z&sp=r&sv=2024-08-04&sr=b&scid=00000000-0000-0000-0000-000000000000&skoid=e9d2f8b1-028a-4cff-8eb1-d0e66fbefcca&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-05-13T10%3A25%3A43Z&ske=2025-05-14T10%3A25%3A43Z&sks=b&skv=2024-08-04&sig=FjV9BnwFAwAPwkL7uuGZAfh/aoRVwxpZIDQj2xyksGs%3D")',
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <div className="w-3/4 h-[70vh] bg-black bg-opacity-70 border border-gray-600 m-5 flex flex-col text-white rounded-lg shadow-lg">
+        <h1 className="p-5 border-b border-gray-600 text-xl font-semibold">Chat</h1>
+
+        <div className="flex-1 overflow-y-auto p-5 space-y-4">
+          {messages.map((msg, index) => (
             <div key={index} className="chat chat-start">
-              <div className="chat-header">
+              <div className="chat-header font-bold">
                 {msg.firstName}
-                <time className="text-xs opacity-50">2 hours ago</time>
+                <time className="text-xs opacity-50 ml-2">now</time>
               </div>
-              <div className="chat-bubble">{msg.text}</div>
+              <div className="chat-bubble text-white">{msg.text}</div>
               <div className="chat-footer opacity-50">Seen</div>
             </div>
-          );
-        })}
-      </div>
-      <div className="p-5 border-t border-gray-600 flex items-center gap-2">
-        <input
-          value={newMessage}
-          onChange={(e) => setNewMessage(e.target.value)}
-          className="flex-1 border border-gray-500 text-white rounded p-2"
-        ></input>
-        <button onClick={sendMessage} className="btn btn-secondary">
-          Send
-        </button>
+          ))}
+        </div>
+
+        <div className="p-5 border-t border-gray-600 flex items-center gap-2">
+          <input
+            value={newMessage}
+            onChange={(e) => setNewMessage(e.target.value)}
+            className="flex-1 border border-gray-500 bg-transparent text-white rounded p-2"
+            placeholder="Type a message..."
+          />
+          <button
+            onClick={sendMessage}
+            className="btn btn-secondary text-white font-semibold px-4 py-2 rounded"
+          >
+            Send
+          </button>
+        </div>
       </div>
     </div>
   );
